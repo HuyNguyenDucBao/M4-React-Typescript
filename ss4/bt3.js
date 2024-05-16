bc830a6bfc02f5dadca2681467f005a1380b18a2
HuyNguyenDucBao <149989165+HuyNguyenDucBao@users.noreply.github.com>
2024-05-16 08:38:58 +0700

    main

--- ss4/bt3.js
+++ ss4/bt3.js
@@ -1,9 +1,17 @@
 function typeConsole(type = "log") {
-    console[type] ? consoletype : console.log('Type không hợp lệ');
+  const validTypes = ["log", "warn", "error", "info", "debug"];
+  if (validTypes.includes(type)) {
+    console[type](`Đây là type: ${type}`);
+  } else {
+    console.log('Type không hợp lệ');
   }
-  
-  // Ví dụ sử dụng hàm
-  typeConsole(); // In ra: Đây là type: log
-  typeConsole('warn'); // In ra: Đây là type: warn
-  typeConsole('error'); // In ra: Đây là type: error
-  
+ }
+ 
+ // Ví dụ sử dụng
+ typeConsole(); // In ra: Đây là type: log
+ typeConsole('warn'); // In ra: Đây là type: warn
+ typeConsole('error'); // In ra: Đây là type: error
+ typeConsole('info'); // In ra: Đây là type: info
+ typeConsole('debug'); // In ra: Đây là type: debug
+ typeConsole('invalidType'); // In ra: Type không hợp lệ
+ 

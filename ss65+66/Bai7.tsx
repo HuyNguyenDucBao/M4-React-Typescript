d966ec3507e4e5cfe5a2eee6959ca96b974f3e64
HuyNguyenDucBao <149989165+HuyNguyenDucBao@users.noreply.github.com>
2024-07-17 07:55:08 +0700

    main

---
+++ ss65+66/Bai7.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StoreInterface } from "../../Store";
+import { color } from "../../Store/actions/bai7";
+
+const Bai7 = () => {
+  const switchColor = useSelector((state: StoreInterface) => state.userStore7);
+  const dispatch = useDispatch();
+  const changeCheck = () => {
+    dispatch(color());
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: switchColor ? "black" : "red",
+      }}
+    >
+      <input type="checkbox" onChange={changeCheck} />
+      <label>{switchColor ? "DO" : "Hong"}</label>
+    </div>
+  );
+};
+
+export default Bai7;
+///
+///////
